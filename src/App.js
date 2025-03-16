e775de8fb14bae2364bf51be17c8a899c03a5a24
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-03-16 15:42:20 +0700

    update font and SplashScreen

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
-import React from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  SafeAreaView
-} from 'react-native';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
+import { SplashScreen } from './pages';
 
 const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Hello Food Market</Text>
-      </SafeAreaView>
-    </>
-  );
+  return <SplashScreen />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
