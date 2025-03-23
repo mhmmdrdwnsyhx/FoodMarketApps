e5f4e704476689b910a3fcc5b7cb82e293071434
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-03-23 14:38:20 +0700

    add Order & Update Home

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { IlOrder } from '../../assets'
+import { Button, Gap } from '../../components'
 
-const Order = () => {
+const Order = ({navigation}) => {
   return (
-    <View>
-      <Text>Order Page</Text>
+    <View style={styles.page}>
+      <IlOrder/>
+      <Gap height={30}/>
+      <Text style={styles.title}>Ouch! Hungry</Text>
+      <Gap height={6}/>
+      <Text style={styles.label}>Seems like you have not</Text>
+      <Text style={styles.label}>ordered any food yet</Text>
+      <Gap height={30}/>
+      <View style={styles.buttonContainer}>
+        <Button text={"Find foods"} onPress={() => ('')}/>
+      </View>
     </View>
   )
 }
 
 export default Order
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Poppins-Regular',
+    color: '#020202'
+  },
+  label: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Light',
+    color: '#8D92A3'
+  },
+  buttonContainer: {
+    width: '100%',
+    paddingHorizontal: 80,
+  },
+})
