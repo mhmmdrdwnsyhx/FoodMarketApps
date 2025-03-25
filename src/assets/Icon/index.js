eacb31641c12a6ded81f736c52592e85bba0dc91
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-03-26 00:12:54 +0700

    Today is the latest update

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -5,5 +5,7 @@ import IcOrderOff from './ic-order-off.svg';
 import IcOrderOn from './ic-order-on.svg';
 import IcProfileOff from './ic-profile-off.svg';
 import IcProfileOn from './ic-profile-on.svg';
+import IcStarOn from './ic-star-on.svg';
+import IcStarOff from './ic-star-off.svg';
 
-export {IcBack, IcHomeOff, IcHomeOn, IcOrderOff, IcOrderOn, IcProfileOff, IcProfileOn};
+export {IcBack, IcHomeOff, IcHomeOn, IcOrderOff, IcOrderOn, IcProfileOff, IcProfileOn, IcStarOn, IcStarOff};
