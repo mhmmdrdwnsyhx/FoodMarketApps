1d580306048e3bbe9a42cab6cbb1504b2ae9ad3b
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-04-05 12:02:07 +0700

    Holiday is over -1

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,6 @@ import SuccessSignUp from "./SuccessSignUp";
 import Home from "./Home";
 import Order from "./Order";
 import Profile from "./Profile";
+import FoodDetail from "./FoodDetail";
 
-export {SplashScreen, SignIn, SignUp, SignUpAddress, SuccessSignUp, Profile, Order, Home, };
+export { SplashScreen, SignIn, SignUp, SignUpAddress, SuccessSignUp, Profile, Order, Home, FoodDetail, };
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Home, Order, Profile, SignIn, SignUp, SignUpAddress, SplashScreen, SuccessSignUp } from "../pages";
+import { FoodDetail, Home, Order, Profile, SignIn, SignUp, SignUpAddress, SplashScreen, SuccessSignUp } from "../pages";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { BottomNavigator } from "../components";
@@ -10,9 +10,9 @@ const Tab = createBottomTabNavigator();
 const MainApp = () => {
     return (
         <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
-            <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
-            <Tab.Screen name="Order" component={Order} options={{headerShown: false}}/>
-            <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}} />
+            <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+            <Tab.Screen name="Order" component={Order} options={{ headerShown: false }} />
+            <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
         </Tab.Navigator>
     )
 };
@@ -21,12 +21,13 @@ const MainApp = () => {
 const Router = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
-            <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
-            <Stack.Screen name="SignUpAddress" component={SignUpAddress} options={{headerShown: false}}/>
-            <Stack.Screen name="SuccessSignUp" component={SuccessSignUp} options={{headerShown: false}}/>
-            <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}}/>
+            <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUpAddress" component={SignUpAddress} options={{ headerShown: false }} />
+            <Stack.Screen name="SuccessSignUp" component={SuccessSignUp} options={{ headerShown: false }} />
+            <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown: false }} />
+            <Stack.Screen name="FoodDetail" component={FoodDetail} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 };
