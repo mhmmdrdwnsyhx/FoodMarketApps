6e71e34394903d5320a4536b1c01cf757ea2865d
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-04-07 00:24:51 +0700

    add Counter

--- src/components/molecules/Counter/index.js
+++ src/components/molecules/Counter/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { IcMin, IcPlus } from '../../../assets'
 
 const Counter = () => {
     return (
-        <View>
-            <Text>Counter</Text>
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <IcMin />
+            </TouchableOpacity>
+            <Text style={styles.value}>14</Text>
+            <TouchableOpacity>
+                <IcPlus />
+            </TouchableOpacity>
         </View>
     )
 }
 
 export default Counter
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    value: {
+        fontSize: 16,
+        fontFamily: 'Poppins-Regular',
+        color: '#202020',
+        marginHorizontal: 10,
+    },
+})
